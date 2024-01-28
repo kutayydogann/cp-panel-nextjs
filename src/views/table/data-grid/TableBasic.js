6696f3aadb2c0697100f9f38b5c0f554ee7fc9c7
kutayydogann <121308811+kutayydogann@users.noreply.github.com>
2024-01-28 17:13:33 +0300

    main

--- src/views/table/data-grid/TableBasic.js
+++ src/views/table/data-grid/TableBasic.js
@@ -1,62 +1,60 @@
-import React, { useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import { DataGrid } from '@mui/x-data-grid';
-import axios from 'axios';
+// ** MUI Imports
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import { DataGrid } from '@mui/x-data-grid'
+import CardHeader from '@mui/material/CardHeader'
+
+// ** Data Import
+import { rows } from 'src/@fake-db/table/static-data'
 
 const columns = [
   {
+    flex: 0.1,
+    field: 'id',
+    minWidth: 80,
+    headerName: 'ID'
+  },
+  {
     flex: 0.25,
     minWidth: 200,
-    field: 'username',
-    headerName: 'Ad Soyad'
+    field: 'full_name',
+    headerName: 'Name'
   },
   {
     flex: 0.25,
     minWidth: 230,
     field: 'email',
-    headerName: 'E-Posta'
+    headerName: 'Email'
   },
-];
+  {
+    flex: 0.15,
+    minWidth: 130,
+    field: 'start_date',
+    headerName: 'Date'
+  },
+  {
+    flex: 0.15,
+    minWidth: 120,
+    field: 'experience',
+    headerName: 'Experience'
+  },
+  {
+    flex: 0.1,
+    field: 'age',
+    minWidth: 80,
+    headerName: 'Age'
+  }
+]
 
 const TableBasic = () => {
-  const [users, setUsers] = useState([]);
-  const [searchValue, setSearchValue] = useState('');
-
-  useEffect(() => {
-    show();
-  }, [searchValue]);
-
-  const show = () => {
-    axios.get("http://localhost/mynextapp/view.php")
-      .then(response => {
-        console.log("Gelen Veri: ", response.data);
-        const filteredUsers = response.data
-          .filter(user => 
-            user.username.toLowerCase().includes(searchValue.toLowerCase()) ||
-            user.email.toLowerCase().includes(searchValue.toLowerCase())
-          )
-          .reverse();
-        setUsers(filteredUsers);
-      })
-      .catch(error => {
-        console.error("Veri Çekme Hatası: ", error);
-      });
-  }
-
-  const handleSearch = (event) => {
-    setSearchValue(event.target.value);
-  };
-
   return (
     <Card>
-      <CardHeader title='Kullanıcı Listesi' />
+      <CardHeader title='Basic' />
       <Box sx={{ height: 500 }}>
-        <DataGrid columns={columns} rows={users} />
+        <DataGrid columns={columns} rows={rows.slice(0, 10)} />
       </Box>
     </Card>
-  );
+  )
 }
 
-export default TableBasic;
+export default TableBasic
